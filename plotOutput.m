4ec4a90291d97d050422e48b7cba6b7667d233cf
Elucidation <elucidation@gmail.com>
2011-12-09 03:45:06 -0800

    added basic animation to matlab

--- plotOutput.m
+++ plotOutput.m
@@ -1,4 +1,10 @@
 clear all; close all; clc;
+MODE_REALTIME = 1;
+MODE_PLOT = 2;
+
+MODE = MODE_REALTIME;
+
+
 filename = 'testOutput.out';
 fprintf('Reading file %s...\n',filename);
 
@@ -7,19 +13,37 @@ f = fopen(filename,'r');
 head = fgetl(f);
 
 disp(head);
-[n,steps,dt] = sscanf(head,'SIMULATION %i BODIES, %i STEPS, %d DT');
+headdata = sscanf(head,'SIMULATION %d BODIES, %d STEPS, %g DT');
+n = headdata(1);
+steps = headdata(2);
+dt = headdata(3);
 
 
 % x y z vx vy vz columns
 data = fscanf(f,'%f %f %f %f %f %f', [6 inf])';
+fclose(f);
 
-figure;
-hold on;
+% GRAPHICS
 colors = 'bgryk';
 getColor = @(x) colors(mod(x,length(colors)));
-for i = 1:n
-    plot(data(i:n:end,1),data(i:n:end,2),  sprintf('.-%c',getColor(i))  );
-end
-hold off;
+figure;
 axis equal;
-fclose(f);
+if (MODE == MODE_PLOT)
+    hold on;    
+    for i = 1:n
+        plot(data(i:n:end,1),data(i:n:end,2),  sprintf('.-%c',getColor(i))  );
+    end
+    hold off;
+elseif (MODE == MODE_REALTIME)
+    for t = 1:(size(data,1)/n)
+        %for i = 1:n
+        %    plot(data((t-1)*n + i,1),data((t-1)*n + i,2),  sprintf('.%c',getColor(i))  );
+        %    hold on;
+        %end
+        plot(data((t-1)*n+1:t*n,1),data((t-1)*n+1:t*n,2),'.k');
+        title(sprintf('%i bodies, %i steps, %g dt, Time: %g/%g',n,steps,dt,t*dt,dt*steps));
+        %hold off;
+        axis([-2 2 -2 2]);
+        pause(0.0001);
+    end
+end
