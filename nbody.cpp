2f563be438aff3b83ccaefbab8823f86c1409646
Elucidation <elucidation@gmail.com>
2011-12-09 01:10:23 -0800

    basic simulate step up

--- nbody.cpp
+++ nbody.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 struct body {
   double x,y,z,vx,vy,vz;
 };
 
+struct v3 {
+  double x,y,z;
+};
+
 void printBody (body b)
 {
-  cout << "Body : P(" << b.x << "," << b.y << "," << b.z << ") ";
-  cout << "V(" << b.vx << "," << b.vy << "," << b.  vz << ")\n";
+  cout << "body < P(" << b.x << "," << b.y << "," << b.z << ") ";
+  cout << "V(" << b.vx << "," << b.vy << "," << b.  vz << ") >\n";
+}
+
+void printBodies(body* bodies,int n)
+{
+  for (int i=0;i<n;i++) {
+    cout << i << " ";
+    printBody(bodies[i]);
+  }
+}
+
+int inputCorrupt()
+{
+  cout << "Corrupt input file\n";
+  return -1;
+}
+
+void step(body* bodies,int n, double dt) {
+  for (int i=0; i < n; i++) {
+    // For each body
+    bodies[i].x = bodies[i].x+bodies[i].vx;
+  }
+}
+
+void simulate(body* bodies,int n, int steps,double dt) {
+  for (int i=1;i<=steps;i++){
+    // For each step
+    step(bodies,n,dt);
+    cout << "Step " << i << ", Time " << i*dt << '\n';
+  }
 }
 
 int main(int argc, char *argv[])
@@ -26,11 +60,7 @@ int main(int argc, char *argv[])
   i = 0;
   while (cin >> k)
   {
-    cout << i << endl;
-    if (i > n-1) {
-      cout << "Corrupt input file\n";
-      return -1;
-    }
+    if (i > n-1) return inputCorrupt();
     bods[i].x = k;
     cin >> bods[i].y;
     cin >> bods[i].z;
@@ -39,14 +69,24 @@ int main(int argc, char *argv[])
     cin >> bods[i].vz;
     i++;
   }
-  if (i != n) {
-    cout << "Corrupt input file\n";
-    return -1;
+  if (i != n) return inputCorrupt();
+  
+  printBodies(bods,n);
+  
+  if (argc == 3) {
+    int steps = atoi(argv[1]);
+    double dt = atof(argv[2]);
+    cout << "Steps to iterate: " << steps << ", dt: " << dt << '\n';
+    simulate(bods,n,steps,dt);
+    printBodies(bods,n);
+  } else {
+    cout << "You can pass in an integer and a double "
+                "number of steps and dt";
   }
   
-  for (i=0;i<n;i++) {
-    printBody(bods[i]);
-  }
+  // free memory used for bodies
+  delete [] bods; 
+  bods = NULL;
   
   cout << "\nDone reading file.";
   return 0;
